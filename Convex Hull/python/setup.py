f6dfece219b3471f40e2ac495b8b5da26b236b6f
Khalil Fazal <khalil.fazal.0@gmail.com>
2012-11-20 12:44:39 -0500

    Packaged the project as a python egg

---
+++ Convex Hull/python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup (
+    name            = 'convexHull',
+    version         = '0.1',
+    description     = 'A solution to the Convex Hull problem using the Monotone Chain Algorithm.',
+    url             = 'https://github.com/capncanuck/AlgorithmsProject',
+    author          = 'Khalil Fazal',
+    author_email    = 'khalil.fazal.0@gmail.com',
+    license         = 'GPL',
+    packages        = ['convexHull'],
+    zip_safe        = False
+)
